b25a29fd68891e29b290e56b395eda2c2bb3a0b2
Hamilcer <68626128+Hamilcer@users.noreply.github.com>
2020-12-19 16:42:25 -0500

    Data table categories working

---
+++ src/views/Categoria.vue
@@ -0,0 +1,13 @@
+<template>
+    <data-table></data-table> 
+</template>
+
+<script>
+import DataTable from '../components/DataTable'
+export default {
+    components:{
+    DataTable
+    }
+}
+</script>
+
